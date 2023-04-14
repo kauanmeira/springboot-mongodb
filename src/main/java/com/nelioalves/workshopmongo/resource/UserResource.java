4ca1f9641b9e9b4a587c8d7b92fb12e87822b09b
kauanmeira <kauangabriel.meira123@gmail.com>
2023-04-14 09:57:31 -0300

    Data instantiation

--- src/main/java/com/nelioalves/workshopmongo/resource/UserResource.java
+++ src/main/java/com/nelioalves/workshopmongo/resource/UserResource.java
@@ -1,30 +1,26 @@
-package com.nelioalves.workshopmongo.resource;
+package com.nelioalves.workshopmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nelioalves.workshopmongo.domain.User;
+import com.nelioalves.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value="/users")
-
 public class UserResource {
-	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll(){
-		User kauan = new User("1", "kauan","kauan@gmail.com");
-		User alex = new User ("2", "alex", "alex@gmail.com");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(kauan,alex));
-		return ResponseEntity.ok().body(list);
-		
-	}
 
+    @Autowired
+    private UserService service;
+
+    @RequestMapping(method=RequestMethod.GET)
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
 }
